01c3462e38aa93e678ec06d7d387c20521b5cc3a
George Hlongwane <georgehlongwane8@gmail.com>
2024-06-04 12:12:11 +0200

    Created app and project URLs

---
+++ ecommerce_project/returns_exchanges/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('', views.dashboard, name='dashboard'),
+    path('return_exchange_request/<int:item_id>/', views.return_exchange_request, name='return_exchange_request'),
+]
