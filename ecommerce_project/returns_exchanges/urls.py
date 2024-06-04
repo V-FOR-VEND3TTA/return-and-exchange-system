f2b4ea564771485402e48090012c97ca065fb0d1
George Hlongwane <georgehlongwane8@gmail.com>
2024-06-04 15:27:34 +0200

    Registered models with the admin

--- ecommerce_project/returns_exchanges/urls.py
+++ ecommerce_project/returns_exchanges/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    # path('logout/', views.user_logout, name='logout'),
     path('', views.dashboard, name='dashboard'),
-    path('return_exchange_request/<int:item_id>/', views.return_exchange_request, name='return_exchange_request'),
+    path('return_exchange_request/<int:item_id>/', views.return_exchange_request, name='returns'),
 ]
