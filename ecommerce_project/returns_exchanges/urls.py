66cbb17a4991de87b91d62e52afc9df62c095193
George Hlongwane <georgehlongwane8@gmail.com>
2024-06-05 06:47:36 +0200

    Created logout view, urls, and link in the base template

--- ecommerce_project/returns_exchanges/urls.py
+++ ecommerce_project/returns_exchanges/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import signup, user_login, dashboard, return_exchange_request
+from .views import signup, user_login, user_logout, dashboard, return_exchange_request
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', user_login, name='login'),
-    # path('logout/', views.user_logout, name='logout'),
+    path('logout/', user_logout, name='logout'),
     path('', dashboard, name='dashboard'),
     path('return_exchange_request/', return_exchange_request, name='return_exchange_request'),
 ]
